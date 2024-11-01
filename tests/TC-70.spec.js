2f7fbeff90f90f7162a4a051983cd0588cab1a4b
Michael Grove <michael.k.grove@saic.com>
2024-11-01 10:38:02 -0400

    C&S 110124

--- tests/TC-70.spec.js
+++ tests/TC-70.spec.js
@@ -8,7 +8,7 @@ test.use({
 });
 
 test('test', async ({ page }) => {
-    test.setTimeout(180000);
+    test.setTimeout(120000);
     await page.goto('https://learn-doc.test.josce.mil/moodle/login/index.php?saml=off');
     await page.getByPlaceholder('Username').click();
     await page.getByPlaceholder('Username').fill('automation_test');
@@ -19,17 +19,18 @@ test('test', async ({ page }) => {
     await page.getByRole('button', { name: 'Log in' }).click();
     await page.waitForTimeout(750);
     // await page.locator('xpath=//*[@id="yui_3_18_1_1_1730320588579_18"]').click();
-    // await page.locator('#nav-bar :text("Toggle search input")').click();
+    await page.locator('#nav-bar :text("Toggle search input")').click();
     // await page.locator('article:has-text("Toggle search input")').click();
     // await page.getByRole('button', { name: 'Toggle search input' }).click();
-    await page.getByRole('textbox', { name: 'Search' }).click();
-    await page.getByRole('textbox', { name: 'Search' }).fill('smith');
-    await page.getByRole('button', { name: 'Search' }).click();
-    await page.getByRole('link', { name: 'Users' }).click();
-    await page.waitForTimeout(750);
-    await page.getByLabel('User menu').click();
-    await page.waitForTimeout(750);
-    await page.getByRole('menuitem', { name: 'Log out' }).click();
-    await page.goto('https://learn-doc.test.josce.mil/moodle/login/index.php?saml=off');
-    await page.waitForTimeout(2000);
+    // await page.getByRole('textbox', { name: 'Search' }).click();
+    await page.getByPlaceholder('Search').click();
+    await page.getByPlaceholder('Search').fill('smith');
+    // await page.getByRole('button', { name: 'Search' }).click();
+    // await page.getByRole('link', { name: 'Users' }).click();
+    // await page.waitForTimeout(750);
+    // await page.getByLabel('User menu').click();
+    // await page.waitForTimeout(750);
+    // await page.getByRole('menuitem', { name: 'Log out' }).click();
+    // await page.goto('https://learn-doc.test.josce.mil/moodle/login/index.php?saml=off');
+    // await page.waitForTimeout(2000);
 });
