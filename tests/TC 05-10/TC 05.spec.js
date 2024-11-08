80e7e2fc5ffe117c0fb823e4dcb44817c7e399ec
Michael Grove <michael.k.grove@saic.com>
2024-11-08 09:15:25 -0500

    Kiwi TC 11082024

--- tests/TC 05-10/TC 05.spec.js
+++ tests/TC 05-10/TC 05.spec.js
@@ -9,6 +9,15 @@ test.use({
 
 
 test('test', async ({ page }) => {
-    test.setTimeout(120000);
-    
+    test.setTimeout(600000);
+    await page.goto('https://learn-doc.test.josce.mil/moodle/login/index.php?saml=off');
+    await page.waitForTimeout(1000);
+    await page.getByPlaceholder('Username').click();
+    await page.getByPlaceholder('Username').fill('automation_test');
+    await page.waitForTimeout(1000);
+    await page.getByPlaceholder('Password').click();
+    await page.getByPlaceholder('Password').fill('Seymour2024$!$!');
+    await page.waitForTimeout(1000);
+    await page.getByRole('button', { name: 'Log in' }).click();
+    await page.waitForTimeout(1000);
 });
